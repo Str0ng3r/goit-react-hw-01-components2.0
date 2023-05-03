93c7e0c742cc229fbe6bd087aa84a21ee48dac8a
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-05-03 18:08:32 +0200

    UPDATE

---
+++ src/components/friends-comp/friendli.jsx
@@ -0,0 +1,6 @@
+import style from './Friends.module.css'
+export const MakeFriends = ({avatar,name,isOnline,id}) => {
+    return (
+        <li className={style.liuser} key={id}><div className={isOnline ? style.redcirc : style.greencirc}></div><img className={style.avataruser} src={avatar} alt='avatar-user' /><p className={style.nameuser}>{name}</p> </li>
+    )
+}
--- src/components/friends-comp/friends.jsx
+++ src/components/friends-comp/friends.jsx
@@ -1,11 +1,12 @@
 import propTypes from "prop-types"
 import style from './Friends.module.css'
+import { MakeFriends } from "./friendli"
 export const ListFriends = ({dataFriend}) => {
     return (
         <section>
         <ul className={style.userslist}>
 {dataFriend.map(({avatar,name,isOnline,id}) => (
-    <li className={style.liuser} key={id}><div className={isOnline ? style.redcirc : style.greencirc}></div><img className={style.avataruser} src={avatar} alt='avatar-user' /><p className={style.nameuser}>{name}</p> </li>
+    <MakeFriends avatar={avatar} name={name} isOnline={isOnline} id={id}></MakeFriends>
 ))}
         </ul>
         </section>
